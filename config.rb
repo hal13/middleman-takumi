d83d9f324640ced8d7e5accf7dd448dc82f1a119
hal13 <s9813180@gmail.com>
2017-01-25 03:52:36 +0900

    modify sprockets

--- config.rb
+++ config.rb
@@ -18,6 +18,8 @@ page '/*.txt', layout: false
 # proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
 #  which_fake_page: 'Rendering a fake page with a local variable' }
 
+activate :sprockets
+
 ###
 # Helpers
 ###
